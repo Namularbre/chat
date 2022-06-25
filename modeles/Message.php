6a73e820fc3f7a954bf314380f7852c08ed8194e
Sazerat <namulabre@gmail.com>
2022-06-25 15:15:34 +0200

    ajout de commentaires pour le modèle des messages

--- modeles/Message.php
+++ modeles/Message.php
@@ -4,17 +4,19 @@ require_once("../bdd/bdd.php");
 
 class Message implements Connectable {
     private BDD $bdd;
-
+    //Lorsque l'on instancie ce modèle, on instancie une interface de connexion à la base de données.
     public function __construct(){
         $this->bdd = $this->connecter();
     }
-
+    //Provient de Connectable. Cette méthode retourne une nouvelle liaison à la base de données
     public function connecter() : BDD{
         return new BDD();
     }
-
+    //Provient de Connectable. Cette méthode en appelle une autre qui vas détruire la connexion à la base.
+    //On détruit ensuite l'interface de connexion.
     public function deconnecter(){
         $this->bdd->deconnecterBDD();
+        unset($this->bdd);
     }
     //Ajoute un message dans la base de données, qui sera lu par les utilisateurs
     public function enregistrerMessage(int $idExpediteur, int $idDestinataire, string $message){
